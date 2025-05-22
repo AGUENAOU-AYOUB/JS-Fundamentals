c910a9589196d96190c3e3976ed1b07525d65aad
Ayoub AGUENAOU <aguenaouayoub04@gmail.com>
2025-05-22 23:10:36 +0100

    arg

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+let argument = '1';
+if (argument.length === 0) {
+    console.log('No argument');
+} else if (argument.length === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Argument found');
+}
