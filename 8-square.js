f5503fa52333cccdf45d314acc271784eda4eced
Ayoub AGUENAOU <aguenaouayoub04@gmail.com>
2025-05-23 02:20:28 +0100

    mission success

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+let myArg = process.argv.slice(2);
+let number = parseInt(myArg);
+
+if (!isNaN(number)) {
+    for (let i = 0; i < number; i++){
+        console.log('X'.repeat(number));
+    }
+} else {
+    console.log('Missin size');
+}
