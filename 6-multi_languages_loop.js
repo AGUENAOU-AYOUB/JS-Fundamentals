c1ea87ed42f6b2d4b7207cf683e4525331dce814
Ayoub AGUENAOU <aguenaouayoub04@gmail.com>
2025-05-23 01:50:52 +0100

    mission succesfull

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+let language = ['C', 'Python', 'JavaScript'];
+let attribute = ['fun', 'cool', 'amazing'];
+
+let i = 0;
+while (i < 3) {
+    console.log(`${language[i]} is ${attribute[i]}`);
+    i++;
+}
